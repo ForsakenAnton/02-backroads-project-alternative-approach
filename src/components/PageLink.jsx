129208c2dac6dad926f803fd739f6918bbc3ca5a
Anton <forsakenanton@gmail.com>
2025-03-18 14:36:52 +0200

    Initial Commit

---
+++ src/components/PageLink.jsx
@@ -0,0 +1,12 @@
+
+const PageLink = ({ link, itemClass }) => {
+    return (
+        <li>
+            <a href={link.href} className={itemClass}>
+                {link.text}
+            </a>
+        </li>
+    );
+};
+
+export default PageLink;
